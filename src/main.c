516f73925ca2105db8d94a88ff1aea9f15977506
Lukasz Grobelczyk <lukaszgrobelczyk@gmail.com>
2023-01-03 17:28:44 +0100

    Some files

--- src/main.c
+++ src/main.c
@@ -2,99 +2,444 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <string.h>
 // it is very important that this define is before fuse.h is included
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
 
-int comi_getattr(const char *path, struct stat *statbuf)
+static void comi_fullpath(char fpath[PATH_MAX], const char *path)
 {
-    return lstat(path, statbuf);
+	// strcpy(fpath, "/home/lukasz/Test/ProjektZespołowy/commifs/src/rootComi");
+	strcpy(fpath, path);
+	// printf("Parts %s\n", path);
+	// strncat(fpath, path, PATH_MAX);
+	// printf("Fullpath %s\n", fpath);
 }
 
-int comi_mkdir(const char *path, mode_t mode)
-{ 
-    return mkdir(path, mode);
-}
-
-int comi_chmod(const char *path, mode_t mode)
+static int xmp_getattr(const char *path, struct stat *stbuf)
 {
-    return chmod(path, mode);
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	// printf("Getattr %s\n", path);
+	res = lstat(fpath, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
 }
 
-int comi_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+static int xmp_access(const char *path, int mask)
 {
-    return pread(fi->fh, buf, size, offset);
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	res = access(fpath, mask);
+	if (res == -1)
+		return -errno;
+
+	return 0;
 }
 
-int comi_write(const char *path, const char *buf, size_t size, off_t offset,
-	     struct fuse_file_info *fi)
-{     
-    return pwrite(fi->fh, buf, size, offset);
-}
-
-int comi_opendir(const char *path, struct fuse_file_info *fi)
+static int xmp_readlink(const char *path, char *buf, size_t size)
 {
-    DIR *dp;
-    int retstat = 0;
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	res = readlink(fpath, buf, size - 1);
+	if (res == -1)
+		return -errno;
 
-    dp = opendir(path);
-    if (dp == NULL) {
-      //TODO
-    }
-    
-    fi->fh = (intptr_t) dp;
-    
-    return retstat;
+	buf[res] = '\0';
+	return 0;
 }
 
-int comi_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
-	       struct fuse_file_info *fi)
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi)
 {
-    DIR *dp;
-    struct dirent *de;
-    dp = (DIR *) (uintptr_t) fi->fh;
-    de = readdir(dp);
-    if (de == 0) {
-      //TODO
-      return -1;
-    }
-    return 0;
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	printf("Readdir %s\n", path);
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		if (filler(buf, de->d_name, &st, 0))
+			break;
+	}
+
+	closedir(dp);
+	return 0;
 }
 
-struct fuse_operations commi_oper = {
-  .getattr = comi_getattr,
-  .readlink = NULL,
-  .getdir = NULL,
-  .mknod = NULL,
-  .mkdir = comi_mkdir,
-  .unlink = NULL,
-  .rmdir = NULL,
-  .symlink = NULL,
-  .rename = NULL,
-  .link = NULL,
-  .chmod = comi_chmod,
-  .chown = NULL,
-  .truncate = NULL,
-  .utime = NULL,
-  .open = NULL,
-  .read = comi_read,
-  .write = comi_write,
-  .statfs = NULL,
-  .flush = NULL,
-  .release = NULL,
-  .fsync = NULL,
-  .opendir = comi_opendir,
-  .readdir = comi_readdir,
-  .releasedir = NULL,
-  .fsyncdir = NULL,
-  .init = NULL,
-  .destroy = NULL,
-  .access = NULL,
-  .ftruncate = NULL,
-  .fgetattr = NULL,
+static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
+	   is more portable */
+	if (S_ISREG(mode)) {
+		res = open(fpath, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if (res >= 0)
+			res = close(res);
+	} else if (S_ISFIFO(mode))
+		res = mkfifo(fpath, mode);
+	else
+		res = mknod(fpath, mode, rdev);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_mkdir(const char *path, mode_t mode)
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	printf("Mkdir %s\n", path);
+	res = mkdir(fpath, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_unlink(const char *path)
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	res = unlink(fpath);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_rmdir(const char *path)
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	res = rmdir(fpath);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_symlink(const char *from, const char *to)
+{
+	int res;
+	char ffrom[PATH_MAX];
+	comi_fullpath(ffrom, from);
+	char fto[PATH_MAX];
+	comi_fullpath(fto, to);
+	res = symlink(ffrom, fto);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_rename(const char *from, const char *to)
+{
+	int res;
+	char ffrom[PATH_MAX];
+	comi_fullpath(ffrom, from);
+	char fto[PATH_MAX];
+	comi_fullpath(fto, to);
+	res = rename(ffrom, fto);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_link(const char *from, const char *to)
+{
+	int res;
+	char ffrom[PATH_MAX];
+	comi_fullpath(ffrom, from);
+	char fto[PATH_MAX];
+	comi_fullpath(fto, to);
+	res = link(ffrom, fto);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_chmod(const char *path, mode_t mode)
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	printf("Chmod +x %s", path);
+	res = chmod(fpath, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_chown(const char *path, uid_t uid, gid_t gid)
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	res = lchown(fpath, uid, gid);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_truncate(const char *path, off_t size)
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	res = truncate(fpath, size);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+#ifdef HAVE_UTIMENSAT
+static int xmp_utimens(const char *path, const struct timespec ts[2])
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	/* don't use utime/utimes since they follow symlinks */
+	res = utimensat(0, fpath, ts, AT_SYMLINK_NOFOLLOW);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+#endif
+
+static int xmp_open(const char *path, struct fuse_file_info *fi)
+{
+	printf("Open %s\n", path);
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	res = open(fpath, fi->flags);
+	if (res == -1)
+		return -errno;
+
+	close(res);
+	return 0;
+}
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	(void) fi;
+	fd = open(fpath, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int xmp_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	printf("Write %s\n", path);
+	fflush(stdout);
+	(void) fi;
+	fd = open(fpath, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int xmp_statfs(const char *path, struct statvfs *stbuf)
+{
+	int res;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	res = statvfs(fpath, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_release(const char *path, struct fuse_file_info *fi)
+{
+	(void) path;
+	(void) fi;
+	return 0;
+}
+
+static int xmp_fsync(const char *path, int isdatasync,
+		     struct fuse_file_info *fi)
+{
+	(void) path;
+	(void) isdatasync;
+	(void) fi;
+	return 0;
+}
+
+#ifdef HAVE_POSIX_FALLOCATE
+static int xmp_fallocate(const char *path, int mode,
+			off_t offset, off_t length, struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+
+	(void) fi;
+
+	if (mode)
+		return -EOPNOTSUPP;
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	fd = open(fpath, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = -posix_fallocate(fd, offset, length);
+
+	close(fd);
+	return res;
+}
+#endif
+
+
+#ifdef HAVE_SETXATTR
+/* xattr operations are optional and can safely be left unimplemented */
+static int xmp_setxattr(const char *path, const char *name, const char *value,
+			size_t size, int flags)
+{
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	int res = lsetxattr(fpath, name, value, size, flags);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+
+static int xmp_getxattr(const char *path, const char *name, char *value,
+			size_t size)
+{
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	int res = lgetxattr(fpath, name, value, size);
+	if (res == -1)
+		return -errno;
+	return res;
+}
+
+static int xmp_listxattr(const char *path, char *list, size_t size)
+{
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	int res = llistxattr(fpath, list, size);
+	if (res == -1)
+		return -errno;
+	return res;
+}
+
+static int xmp_removexattr(const char *path, const char *name)
+{
+	char fpath[PATH_MAX];
+	comi_fullpath(fpath, path);
+	int res = lremovexattr(fpath, name);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+#endif
+
+static struct fuse_operations xmp_oper = {
+	.getattr	= xmp_getattr,
+	.access		= xmp_access,
+	.readlink	= xmp_readlink,
+	.readdir	= xmp_readdir,
+	.mknod		= xmp_mknod,
+	.mkdir		= xmp_mkdir,
+	.symlink	= xmp_symlink,
+	.unlink		= xmp_unlink,
+	.rmdir		= xmp_rmdir,
+	.rename		= xmp_rename,
+	.link		= xmp_link,
+	.chmod		= xmp_chmod,
+	.chown		= xmp_chown,
+	.truncate	= xmp_truncate,
+#ifdef HAVE_UTIMENSAT
+	.utimens	= xmp_utimens,
+#endif
+	.open		= xmp_open,
+	.read		= xmp_read,
+	.write		= xmp_write,
+	.statfs		= xmp_statfs,
+	.release	= xmp_release,
+	.fsync		= xmp_fsync,
+#ifdef HAVE_POSIX_FALLOCATE
+	.fallocate	= xmp_fallocate,
+#endif
+#ifdef HAVE_SETXATTR
+	.setxattr	= xmp_setxattr,
+	.getxattr	= xmp_getxattr,
+	.listxattr	= xmp_listxattr,
+	.removexattr	= xmp_removexattr,
+#endif
 };
 
+// static const struct fuse_operations commi_oper = {
+// 	.getattr	= comi_getattr,
+// 	.readdir	= comi_readdir,
+// 	.open		= comi_open,
+// 	.read		= comi_read,
+// };
 
 int main(int argc, char *argv[]) {
-  fuse_main( argc, argv, &commi_oper, NULL);
+
+	// printf("%s\n", argv[argc-1]);
+	// printf("%s\n", realpath(argv[argc-1], NULL));
+
+	// argv[argc-2] = argv[argc-1];
+	// argv[argc-1] = NULL;
+	// argc--;
+  fuse_main( argc, argv, &xmp_oper, NULL);
 }
